d1c4a513d55e427f2d536e02651eb16b2a253bb6
Kimginam97 <rlarlska79@naver.com>
2022-06-13 10:23:19 +0900

    javascript 와 react 차이

--- session03(리액트 기초및 실습 컴포넌트)/01-starting-setup/src/App.js
+++ session03(리액트 기초및 실습 컴포넌트)/01-starting-setup/src/App.js
@@ -1,7 +1,14 @@
 function App() {
+  // javascript 방식
+  // const para = document.createElement('p');
+  // para.textContent = 'This is also visible';
+  // document.getElementById('root').append(para);
+
   return (
     <div>
       <h2>Let's get started!</h2>
+      {/* 리액트방식 */}
+      <p>This is also visible!</p>
     </div>
   );
 }
