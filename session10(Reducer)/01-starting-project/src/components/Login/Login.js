4a7279b71a996be665a2f4341cdbc31a3dc478b8
Kimginam97 <rlarlska79@naver.com>
2022-06-24 09:47:05 +0900

    useReducer & useEffect

--- session10(Reducer)/01-starting-project/src/components/Login/Login.js
+++ session10(Reducer)/01-starting-project/src/components/Login/Login.js
@@ -14,11 +14,21 @@ const emailReducer = (state, action) => {
     return { value: '', isValid: false };
 };
 
+const passwordReducer = (state, action) => {
+    if (action.type === 'USER_INPUT') {
+        return { value: action.val, isValid: action.val.trim().length > 6 };
+    }
+    if (action.type === 'INPUT_BLUR') {
+        return { value: state.value, isValid: state.value.trim().length > 6 };
+    }
+    return { value: '', isValid: false };
+};
+
 const Login = (props) => {
     // const [enteredEmail, setEnteredEmail] = useState('');
     // const [emailIsValid, setEmailIsValid] = useState();
-    const [enteredPassword, setEnteredPassword] = useState('');
-    const [passwordIsValid, setPasswordIsValid] = useState();
+    // const [enteredPassword, setEnteredPassword] = useState('');
+    // const [passwordIsValid, setPasswordIsValid] = useState();
     const [formIsValid, setFormIsValid] = useState(false);
 
     const [emailState, dispatchEmail] = useReducer(emailReducer, {
@@ -26,43 +36,47 @@ const Login = (props) => {
         isValid: null,
     });
 
-    // useEffect(() => {
-    //     console.log('EFFECT RUNNING');
-    //     return () => {
-    //         console.log('EFFECT CLEANUP');
-    //     };
-    // }, [enteredPassword]);
+    const [passwordState, dispatchPassword] = useReducer(passwordReducer, {
+        value: '',
+        isValid: null,
+    });
 
-    // useEffect(() => {
-    //     const identifier = setTimeout(() => {
-    //         console.log('Checking form validity!');
-    //         setFormIsValid(
-    //             enteredEmail.includes('@') && enteredPassword.trim().length > 6
-    //         );
-    //     }, 500);
-    //     clearTimeout();
+    useEffect(() => {
+        return () => {
+            console.log('EFFECT CLEANUP');
+        };
+    }, []);
 
-    //     return () => {
-    //         console.log('CLEANUP');
-    //         clearTimeout(identifier);
-    //     };
-    // }, [enteredEmail, enteredPassword]);
+    const { isValid: emailIsValid } = emailState;
+    const { isValid: passwordIsValid } = passwordState;
+
+    useEffect(() => {
+        const identifier = setTimeout(() => {
+            console.log('Checking form validity!');
+            setFormIsValid(emailIsValid && passwordIsValid);
+        }, 500);
+        clearTimeout();
+
+        return () => {
+            console.log('CLEANUP');
+            clearTimeout(identifier);
+        };
+    }, [emailIsValid, passwordIsValid]);
 
     const emailChangeHandler = (event) => {
         dispatchEmail({ type: 'USER_INPUT', val: event.target.value });
 
-        setFormIsValid(
-            event.target.value.includes('@') &&
-                enteredPassword.trim().length > 6
-        );
+        // setFormIsValid(
+        //     event.target.value.includes('@') && passwordState.isValid
+        // );
     };
 
     const passwordChangeHandler = (event) => {
-        setEnteredPassword(event.target.value);
+        dispatchPassword({ type: 'USER_INPUT', val: event.target.value });
 
-        setFormIsValid(
-            emailState.isValid && event.target.value.trim().length > 6
-        );
+        // setFormIsValid(
+        //     emailState.isValid && event.target.value.trim().length > 6
+        // );
     };
 
     const validateEmailHandler = () => {
@@ -71,51 +85,27 @@ const Login = (props) => {
     };
 
     const validatePasswordHandler = () => {
-        setPasswordIsValid(enteredPassword.trim().length > 6);
+        dispatchPassword({ type: 'INPUT_BLUR' });
     };
 
     const submitHandler = (event) => {
         event.preventDefault();
-        props.onLogin(emailState.value, enteredPassword);
+        props.onLogin(emailState.value, passwordState.value);
     };
 
     return (
         <Card className={classes.login}>
             <form onSubmit={submitHandler}>
-                <div
-                    className={`${classes.control} ${
-                        emailState.isValid === false ? classes.invalid : ''
-                    }`}
-                >
+                <div className={`${classes.control} ${emailState.isValid === false ? classes.invalid : ''}`}>
                     <label htmlFor="email">E-Mail</label>
-                    <input
-                        type="email"
-                        id="email"
-                        value={emailState.value}
-                        onChange={emailChangeHandler}
-                        onBlur={validateEmailHandler}
-                    />
+                    <input type="email" id="email" value={emailState.value} onChange={emailChangeHandler} onBlur={validateEmailHandler} />
                 </div>
-                <div
-                    className={`${classes.control} ${
-                        passwordIsValid === false ? classes.invalid : ''
-                    }`}
-                >
+                <div className={`${classes.control} ${passwordState.isValid === false ? classes.invalid : ''}`}>
                     <label htmlFor="password">Password</label>
-                    <input
-                        type="password"
-                        id="password"
-                        value={enteredPassword}
-                        onChange={passwordChangeHandler}
-                        onBlur={validatePasswordHandler}
-                    />
+                    <input type="password" id="password" value={passwordState.value} onChange={passwordChangeHandler} onBlur={validatePasswordHandler} />
                 </div>
                 <div className={classes.actions}>
-                    <Button
-                        type="submit"
-                        className={classes.btn}
-                        disabled={!formIsValid}
-                    >
+                    <Button type="submit" className={classes.btn} disabled={!formIsValid}>
                         Login
                     </Button>
                 </div>
