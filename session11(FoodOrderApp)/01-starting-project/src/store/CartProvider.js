1a325cbfe484dfa9edc0ab6a9c80070c448ed551
Kimginam97 <rlarlska79@naver.com>
2022-07-07 15:06:18 +0900

    아이템을 이동식으로 만들기

--- session11(FoodOrderApp)/01-starting-project/src/store/CartProvider.js
+++ session11(FoodOrderApp)/01-starting-project/src/store/CartProvider.js
@@ -30,6 +30,25 @@ const cartReducer = (state, action) => {
       updatedItems = state.items.concat(action.item);
     }
 
+    if (action.type === 'REMOVE') {
+      const existingCartItemIndex = state.items.findIndex(
+        (item) => item.id === action.item.id
+      );
+      const existingItem = state.items[existingCartItemIndex];
+      const updatedTotalAmount = state.totalAmount - existingItem.price;
+      let updatedItem;
+      if (existingItem.amount === 1) {
+        updatedItems = state.items.filter((item) => item.id !== action.id);
+      } else {
+        const updatedItem = {
+          ...existingItem,
+          amount: existingItem.amount - 1,
+        };
+        updatedItems = [...state.items];
+        updatedItems[existingCartItemIndex] = updatedItem;
+      }
+    }
+
     return {
       items: updatedItems,
       totalAmount: updatedTotalAmount,
